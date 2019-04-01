82b23838cd5e2f5b48a8693bae6c1580096689bf
David <david@nedved.com.au>
2019-04-01 22:39:31 +1000

    update with curl included for health checking

--- .setup-multi-arch-build.sh
+++ .setup-multi-arch-build.sh
@@ -4,9 +4,21 @@
 #       https://billglover.me/2018/10/30/multi-architecture-docker-builds/
 
 # Setup multi-architecture docker..
-docker manifest create bushrangers/ha-monitor-api \
+docker manifest create --amend bushrangers/ha-monitor-api \
     bushrangers/ha-monitor-api:arm32v6 \
     bushrangers/ha-monitor-api:arm64v8 \
     bushrangers/ha-monitor-api:amd64
 
 docker manifest push bushrangers/ha-monitor-api:latest
+
+# Build with:
+
+# docker build --no-cache -t bushrangers/ha-monitor-api:arm32v6 -f Dockerfile-arm32v6 .
+# docker build --no-cache -t bushrangers/ha-monitor-api:arm64v8 -f Dockerfile-arm64v8 .
+# docker build --no-cache -t bushrangers/ha-monitor-api:amd64 -f Dockerfile-amd64 .
+
+# Push with:
+
+# docker push bushrangers/ha-monitor-api:arm32v6
+# docker push bushrangers/ha-monitor-api:arm64v8
+# docker push bushrangers/ha-monitor-api:amd64
