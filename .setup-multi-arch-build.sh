13b88675737931b3257c2ccf7ae96a4367ad92b1
David <david@nedved.com.au>
2019-04-01 18:19:36 +1000

    include multi-arch docker setup script

---
+++ .setup-multi-arch-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Please refer to the following URL for further details re multi-architecture docker builds:
+#       https://billglover.me/2018/10/30/multi-architecture-docker-builds/
+
+# Setup multi-architecture docker..
+docker manifest create bushrangers/ha-monitor-api \
+    bushrangers/ha-monitor-api:arm32v6 \
+    bushrangers/ha-monitor-api:arm64v8 \
+    bushrangers/ha-monitor-api:amd64
